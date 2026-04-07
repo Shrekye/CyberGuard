8f4850054a7da69f488a077e8708b17725925c39
erwann-dms <erwann-dms>
2026-04-07 21:24:17 +0200

    Add session import to routes.py for CSRF token management

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 import os
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, session
 from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User, RedTopic, BlueTopic, PurpleTopic, Post
