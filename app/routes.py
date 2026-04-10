18a1d2a7ad9867f6441a57bcb6eb354c4f121783
Sl4yZ2 <acassetcarlot@guardiaschool.fr>
2026-04-10 15:46:32 +0200

    semgrep patch

--- app/routes.py
+++ app/routes.py
@@ -149,6 +149,8 @@ def login():
 @main_bp.route('/login/google')
 def google_login():
     redirect_uri = url_for('main.google_authorize', _external=True, _scheme="https")
+    # nosemgrep: flask-url-for-external-true
+    redirect_uri = url_for('main.google_authorize', _external=True, _scheme="https")
     return google.authorize_redirect(redirect_uri)
 
 
